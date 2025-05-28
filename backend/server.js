445c5bd78aff0fcb1136258e4bff5030fbb5299d
Mukesh <studentmukesh434@gmail.com>
2025-05-28 17:47:04 +0545

    port error fixed

--- backend/server.js
+++ backend/server.js
@@ -240,6 +240,6 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(5000, () => {
-  console.log("server running");
-});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
